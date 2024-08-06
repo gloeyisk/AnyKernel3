0a01e33eaca17bdba3837b2b8f916d63f23287aa
Gloeyisk <gloeyisk@gmail.com>
2024-08-06 07:39:10 +0700

    Update kernel string
    Signed-off-by: Gloeyisk <gloeyisk@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Perf Kernel w/ KernelSU by gloeyisk
+kernel.string=GL-DP Kernel by gloeyisk
 do.devicecheck=1
 do.modules=0
 do.systemless=1
